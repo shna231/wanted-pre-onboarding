ee6e6638423a3acf8dd0d9ffe49d33ae2ee25ef2
shna231 <shna231@gmail.com>
2022-08-13 06:36:00 +0900

    feat: add company

--- lib/company/company.controller.ts
+++ lib/company/company.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { GetCompanyDTO } from './company.dto';
+import { AddCompanyDTO } from './company.dto';
 import { CompanyService } from './company.service';
 import { ApiTags } from '@nestjs/swagger';
+import { debug } from 'console';
 
 @Controller('company')
 @ApiTags('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
-  @Get()
+  @Post('add')
+  insertOne(@Body() req: AddCompanyDTO) {
+    debug('debug print test');
+    debug(req.name);
+    return this.companyService.insertOne(req);
+  }
+
+  @Get('all')
   async getAll(): Promise<GetCompanyDTO> {
     return this.companyService.getAll();
   }
--- lib/company/company.service.ts
+++ lib/company/company.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { AddCompanyDTO } from './company.dto';
 import { GetCompanyDTO } from './company.dto';
 import { Repository } from 'typeorm';
 import { Company } from '../company/company.entity';
@@ -10,6 +11,14 @@ export class CompanyService {
     @InjectRepository(Company)
     private readonly companyRepository: Repository<Company>,
   ) {}
+
+  async insertOne(req: AddCompanyDTO) {
+    return await this.companyRepository.insert({
+      name: req.name,
+      contry: req.contry,
+      region: req.region,
+    });
+  }
 
   async getAll(): Promise<GetCompanyDTO> {
     const response = await this.companyRepository.find();
